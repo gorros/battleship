208ecc4ae31a72a336352d4acda27d9115d974ac
Gor Hayrapetyan <gor.hayrapetyan@gmail.com>
2016-08-13 23:13:39 +0400

    Remove health check from signal.

--- game/signals.py
+++ game/signals.py
@@ -6,6 +6,4 @@ from django.dispatch import receiver
 
 @receiver(pre_save)
 def pre_save_handler(sender, instance, *args, **kwargs):
-    if sender == models.Ship and not instance.health:
-        instance.health = instance.category
     instance.full_clean()
